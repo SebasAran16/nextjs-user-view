1de7276a423890c67e99a1567aff79072b70b47b
Sebas Arango <sebastianarango201316@hotmail.com>
2024-01-03 22:13:44 +0100

    Added favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,13 @@ import { NextIntlClientProvider, useMessages } from "next-intl";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
-  title: "Customer View",
+  title: "CustomerView",
   description: "Stayed linked never easier",
+  icons: {
+    icon: "/favicons/favicon.ico",
+    apple: "/favicons/apple-touch-icon.png",
+  },
+  authors: [{ name: "OwnTheBlock Solutions", url: "" }],
 };
 
 interface RootLayoutProps {
